5963873fa3a24cbd070a3ad16493d4c67355bafe
dlodge440 <101800767+dlodge440@user.noreply.github.com>
2024-03-23 16:31:23 +0100

    Exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+
+    //Check if input is a string
+    if (typeof inputString != 'string'){return "ERROR"};
+
+    //Convert to array, reverse order and reconvert to string
+    const inputArray = inputString.split("");
+    const reversedArray = inputArray.reverse();
+    const reversedString = reversedArray.join(""); 
+    console.log(reversedString);
+    return reversedString;
 
 };
 
